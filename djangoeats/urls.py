a2d81dff51f8eff51dba61db71bdf7f572da2c01
MSH1019 <2833753s@student.gla.ac.uk>
2025-03-13 17:20:27 +0000

    worked on views.py and urls.py

--- djangoeats/urls.py
+++ djangoeats/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from djangoeats import views
 
-app_name = 'djanoeats'
+app_name = 'djangoeats'
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('login/',views.login,name='login'),
-    path('register/',views.register,name='register'),
+    path('login/',views.login_view,name='login'),
+    path('register/',views.register_view,name='register'),
     path('restaurant/<restaurant_slug>/',views.restaurant,name='restaurant'),
     path('restaurant/<restaurant_slug>/review',views.make_review,name='make_review'),
     path('dashboard/',views.dashboard,name='dashboard'),
--- djangoeats/views.py
+++ djangoeats/views.py
@@ -1,33 +1,72 @@
-from django.shortcuts import render # type: ignore
-from django.http import HttpResponse # type: ignore
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Restaurant, MenuItem, Review, UserFavorites, Profile
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+
+
 def home(request):
-    #Display home page
-    return render(request, 'djangoeats/home.html')
+    restaurants = Restaurant.objects.all()
+    return render(request, 'djangoeats/home.html', {'restaurants' : restaurants})
 
-def login(request):
-    #Login functionality for both users and owners?
-    return HttpResponse("Login here")
+#Login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoeats:home')
+        else:
+            return render(request, 'djangoeats/login.html', {'error': 'Invalid Username or Password'})
+    return render(request, 'djangoeats/login.html')
 
-def register(request):
-    #register functionality for both users and owners?
-    return HttpResponse("Register here")
+#Register
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('djangoeats:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'djangoeats/register.html', {'form': form})
 
-def restaurant(request):
-    #Load in details about the given restaurant
-    return HttpResponse("This page will display a specific restaurant")
+
+def restaurant_detail(request, restaurant_slug):
+    restaurant = get_object_or_404(Restaurant, slug=restaurant_slug)
+    menu_items = restaurant.menu_items.all()
+    reviews = restaurant.reviews.all()
+    return render(request, 'djangoeats/restaurant_detail.html', {
+        'restaurant': restaurant,
+        'menu_items': menu_items,
+        'reviews': reviews
+    })
 
 def make_review(request):
-    return HttpResponse("Here you can make a review about a restauraunt")
+    return HttpResponse("leave a review about a restauraunt")
 
 def dashboard(request):
-    #Logic that returns a specific dashboard page based on the user logged in
-    return HttpResponse("Here's your dashboard. View your favourites or update your restaurants")
+    profile = Profile.objects.get(user=request.user)
+    if profile.user_type != "owner":
+        return redirect('djangoeats:home') # go to home page.
+
+    restaurants = Restaurant.objects.filter(owner=request.user)
+    return render(request, 'djangoeats/dashboard.html', {'restaurants': restaurants})
 
 def restaurant_edit(request):
-    #If not the owner display an error page
     return HttpResponse("Update a restaurant that you own here!")
 
+
+@login_required
+def make_review(request, restaurant_slug):
+    restaurant = get_object_or_404(Restaurant, slug=restaurant_slug)
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        rating = int(request.POST.get('rating'))
+        Review.objects.create(reviewer=request.user, restaurant=restaurant, rating=rating, comment=comment)
+        return redirect('djangoeats:restaurant_detail', restaurant_slug=restaurant_slug)
+    return render(request, 'djangoeats/make_review.html', {'restaurant': restaurant})
